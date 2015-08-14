0f5759c74e83a536ea118c6bebb9865f570ecc92
Brian Rowe <briprowe@yahoo.com>
2015-08-14 12:00:35 -0400

    Initial commit of database credentials

---
+++ src/gdd_service/db.clj
@@ -0,0 +1,11 @@
+(ns gdd-service.db
+  (:require [clojure.java.jdbc :as j]))
+
+(def postgress-connection
+  {:subprotocol "postgresql"
+   :subname "//brian-hackday.czas5cnicq66.us-east-1.rds.amazonaws.com:5432/farmlogs_rtma_data"
+   :user "gf8gtyxqredf9q6"
+   :password "zd6vgwc3i2zkpgs"})
+
+(j/query postgress-connection
+         ["select * from rtma_grids"])
